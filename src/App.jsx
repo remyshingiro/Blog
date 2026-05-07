27b0745ab5fcc941cf1ecc72208795e84c94dd47
Remy <shingiroremy303@gmail.com>
2026-05-07 18:40:16 +0200

    garage inventory system

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import PostDetail from "./pages/PostDetail";
-import CreatePost from "./pages/CreatePost";
-import { useState } from "react";
-import {v4 as uuidv4} from "uuid";
-import { useEffect } from "react";
-
-
 function App() {
-  //fn to add new post to the posts array and updates it
-  function addPost(post){
-    // we used previous post to keep up with the latest state
-   setPosts((prevPosts)=> [...prevPosts, post]);
-  }
-  
-  const [posts, setPosts] = useState(
-   ()=>{
-    let savedPosts = localStorage.getItem('all_posts')
-     
-    return savedPosts ? JSON.parse(savedPosts) : []
-   }
-  )
-  useEffect(()=>{
-    localStorage.setItem('all_posts', JSON.stringify(posts))
-  }, [posts])
-   
-
-
-
-
   return (
-    <BrowserRouter>
-      {/* Navbar stays on all pages */}
-      <Navbar />
-
-      {/* Page Content */}
-      <div className="max-w-4xl mx-auto px-4 py-6">
-        <Routes>
-          <Route path="/" element={<Home posts={posts} />} />
-          <Route path="/post/:id" element={<PostDetail />} />
-          <Route path="/create" element={<CreatePost addPost={addPost}/>} />
-        </Routes>
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <div className="text-center">
+        <h1 className="text-5xl font-extrabold text-white tracking-tight">
+          Garage<span className="text-blue-500">Flow</span>
+        </h1>
+        <p className="text-slate-400 mt-4 text-lg">
+          Ready to build a winning inventory system.
+        </p>
       </div>
-    </BrowserRouter>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
